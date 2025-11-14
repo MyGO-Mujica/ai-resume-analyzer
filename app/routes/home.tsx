7a20eb29f35289ba348aa01b1aab1b234661e723
mygo <1158270624@qq.com>
2025-11-14 17:55:11 +0800

    ✨ feat: 实现主页简历卡片展示功能

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,31 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "MyGo" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar></Navbar>
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1> 跟踪您的申请和简历评分</h1>
+        <h2>审核提交内容并查看ai评分结果</h2>
+      </div>
+
+       {resumes.length > 0 && (
+    <div className="resumes-section">
+      {resumes.map((resume) => (
+        <ResumeCard key={resume.id} resume={resume}></ResumeCard>
+     ))}
+    </div>
+  )}
+    </section>
+  </main>
 }
