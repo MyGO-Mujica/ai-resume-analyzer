7e4edf993e73d8353773029f278f14adbbeae482
mygo <1158270624@qq.com>
2025-11-21 22:04:11 +0800

    ✨feat:从云端获取真正的简历并渲染在首页

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,28 +1,44 @@
 import { Link } from "react-router";
 import ScoreCircle from "./ScoreCircle";
+import { useEffect, useState } from "react";
+import { usePuterStore } from "~/lib/puter";
 
 const ResumeCard = ({resume: {id, companyName, jobTitle, feedback, imagePath}}: {resume: Resume}) => {
+  const { fs} = usePuterStore();
+  const [resumeUrl, setResumeUrl] = useState('');
+  useEffect(() => {
+    const loadResume = async () => {
+      const blpb = await fs.read(imagePath)
+      if(!blpb) return;
+      const url = URL.createObjectURL(blpb);
+      setResumeUrl(url);
+    }
+    loadResume();
+  }, [imagePath])
+
   return <Link to ={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
      <div className="resume-card-header">
          <div className="flex flex-col gap-2">
-             <h2 className="text-black! font-bold wrap-break-words">
+            {companyName && <h2 className="text-black! font-bold wrap-break-words">
                {companyName}
-             </h2>
-             <h3 className="text-lg wrap-break-words">{jobTitle}</h3>
+             </h2>}
+             {jobTitle && <h3 className="text-lg wrap-break-words">{jobTitle}</h3>}
+             {!companyName && !jobTitle && <h2 className="text-gray-500 font-bold">未提供公司或职位信息</h2>}
          </div>
          <div className="shrink-0">
             <ScoreCircle score={feedback.overallScore}></ScoreCircle>
          </div>
      </div>
-     <div className="gradient-border animate-in fade-in duration-1000">
+     {resumeUrl && (<div className="gradient-border animate-in fade-in duration-1000">
           <div className="w-full h-full">
                 <img 
-                    src={imagePath} 
+                    src={resumeUrl} 
                     alt="resume"
                     className="w-full h-[350px] max-sm:h-[250px] object-cover object-top"
                     />
           </div>
      </div>
+)}
    </Link>;
 }
 
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,9 +1,8 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
-import { useEffect } from "react";
-import {  useNavigate } from "react-router";
+import { useEffect, useState } from "react";
+import {  useNavigate, Link } from "react-router";
 import { usePuterStore } from "~/lib/puter";
 
 export function meta({}: Route.MetaArgs) {
@@ -14,8 +13,10 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
- const { auth } = usePuterStore();
-  
+ const { auth, kv} = usePuterStore();
+ const navigate = useNavigate();
+ const [resumes, setResumes] = useState<Resume[]>([]);
+ const [LoadingResume, setLoadingResume] = useState(false);
   /**
    * 路由守卫 - 保护需要登录的页面
    * 
@@ -32,27 +33,57 @@ export default function Home() {
    * 依赖监听：
    * - auth.isAuthenticated 变化时重新检查
    */
-  const navigate = useNavigate();
+  
    useEffect(() => {
     if(!auth.isAuthenticated) navigate('/auth?next=/');
   }, [auth.isAuthenticated])
 
+   useEffect(() => {
+    const loadResumes = async () => {
+      setLoadingResume(true);
 
+      const resumes = (await kv.list( 'resume:*',true)) as KVItem[]
+
+      const parsedResumes = resumes?.map(resume => (
+          JSON.parse(resume.value) as Resume
+      ))
+      console.log('parsedResumes', parsedResumes);
+      setResumes(parsedResumes || []);
+      setLoadingResume(false);
+    }
+    loadResumes();
+  }, []);
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar></Navbar>
     <section className="main-section">
       <div className="page-heading py-16">
         <h1> 跟踪您的申请和简历评分</h1>
-        <h2>审核提交内容并查看ai评分结果</h2>
-      </div>
+        {!LoadingResume && resumes.length === 0 ?(
+          <h2 className="text-gray-500 mt-4">您还没有提交任何简历，快去上传第一份简历吧！</h2>
+        ):(
+            <h2>审核提交内容并查看ai评分结果</h2>
+        )}
+        </div>
+        {LoadingResume && (
+          <div className="flex flex-col items-center justify-center">
+              <img src="/images/resume-scan-2.gif" alt="Loading..." className="w-[200px]" />
+          </div>
+        )}
 
-       {resumes.length > 0 && (
+       {!LoadingResume && resumes.length > 0 && (
     <div className="resumes-section">
       {resumes.map((resume) => (
         <ResumeCard key={resume.id} resume={resume}></ResumeCard>
      ))}
     </div>
   )}
+    {!LoadingResume && resumes.length === 0 && (
+      <div className="flex flex-col items-center justify-center mt-10 gap-4">
+        <Link to="/upload" className="primary-button w-fit text-xl font-semibold"
+        >上传第一份简历
+        </Link>
+        </div>
+    )}
     </section>
   </main>
 }
